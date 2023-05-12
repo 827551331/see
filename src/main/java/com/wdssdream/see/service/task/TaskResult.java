2eebad286152181de9fe226dcae7bee70c33bb24
watermelon_juice <827551331@qq.com>
2023-05-12 22:50:53 +0800

    增加 taskResult

---
+++ src/main/java/com/wdssdream/see/service/task/TaskResult.java
@@ -0,0 +1,10 @@
+package com.wdssdream.see.service.task;
+
+/**
+ * description: 每个 task 运行之后都会生成一个 taskResult 作为推送的依据和信息
+ * date: 2023/5/12 22:49
+ *
+ * @author wang_yw
+ */
+public class TaskResult {
+}
