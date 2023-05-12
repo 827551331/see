679253215e2f350e962c42f55f76eadb976e5914
watermelon_juice <827551331@qq.com>
2023-05-13 00:11:25 +0800

    增加属性

--- src/main/java/com/wdssdream/see/service/task/TaskResult.java
+++ src/main/java/com/wdssdream/see/service/task/TaskResult.java
@@ -7,4 +7,11 @@ package com.wdssdream.see.service.task;
  * @author wang_yw
  */
 public class TaskResult {
+
+    /**
+     * 这里用 Integer 方便以后扩展
+     * 若为 1 则直接推送，其他情况留作扩展
+     */
+    private Integer pushFlag;
+
 }
