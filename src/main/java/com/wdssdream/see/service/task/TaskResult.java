e7ccb1104913f059c34df043b8a80d3f591925c2
watermelon_juice <827551331@qq.com>
2023-05-13 23:49:31 +0800

    完善 result

--- src/main/java/com/wdssdream/see/service/task/TaskResult.java
+++ src/main/java/com/wdssdream/see/service/task/TaskResult.java
@@ -1,5 +1,7 @@
 package com.wdssdream.see.service.task;
 
+import com.wdssdream.see.service.message.Message;
+
 /**
  * description: 每个 task 运行之后都会生成一个 taskResult 作为推送的依据和信息
  * date: 2023/5/12 22:49
@@ -14,4 +16,26 @@ public class TaskResult {
      */
     private Integer pushFlag;
 
+    /**
+     * 消息体
+     */
+    private Message message;
+
+    public Integer getPushFlag() {
+        return pushFlag;
+    }
+
+    public TaskResult setPushFlag(Integer pushFlag) {
+        this.pushFlag = pushFlag;
+        return this;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public TaskResult setMessage(Message message) {
+        this.message = message;
+        return this;
+    }
 }
